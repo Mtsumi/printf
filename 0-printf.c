28ed26581730979649cd384f7abff8f9c18326cf
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-19 12:14:40 -0700

    testing new algo

--- 0-printf.c
+++ 0-printf.c
@@ -1,42 +1,47 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
 #include "main.h"
+	
 /**
- * _printf : writes output to stdout
- * handles c, s and % conversion specifiers
- * *format : a character string composed of zero or more directives
- * return -the number of characters printed
- * (excluding the null byte used to end the output to string)
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	int i;
-	int x = strlen(format);
-	va_list apt;
-	va_start(apt, format);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	for (i = 0; i < x; i++)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if(format[i] == (%%c))
+		if (*p == '%')
 		{
-			char y = va_arg(apt, char);
-		_putchar(y);
-		}
-		else if(format[i] == (%%s))
-		{
-			char y = va_arg(apt, char);
-			_putchar(y);
-		}
-		else(format[i] == (%%))
-		{
-			char y = va_arg(apt, char);
-			 _putchar(y);
-		}
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(apt);
-
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
-		
-
-
