34d379b84bab3508f94a5770e36a7dbe2abde73b
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-19 03:25:47 -0700

    putchar

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * _putchar-writes the character c to stdout
+ * @c:The character to print
+ * Return:On success 1.
+ * on error,-1 is returned,and error is set appropriately.
+ */
+int _putchar(char s){
+	return(write(1, &s, 1));
+
+}
