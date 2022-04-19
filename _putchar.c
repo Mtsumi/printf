7688a1f2dd4b9b69ead31991929fe0e734487632
Mtsumi <mtsumijr@gmail.com>
2022-04-19 19:49:03 +0000

    checking with betty

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * _putchar-writes the character c to stdout
- * @c:The character to print
+ * _putchar-writes the character s to stdout
+ * @s:The character to print
  * Return:On success 1.
  * on error,-1 is returned,and error is set appropriately.
  */
-int _putchar(char s){
-	return(write(1, &s, 1));
+int _putchar(char s)
+{
+	return (write(1, &s, 1));
 
 }
